8892ae77bf122c9faad125f579dd1538867005c1
Drassleon <toconasalvaro@gmail.com>
2019-05-20 09:45:47 -0500

    Initial Commit

---
+++ src/main/java/edu/pe/upc/MoneyPlanBackEndApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.upc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyPlanBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyPlanBackEndApplication.class, args);
+	}
+
+}
