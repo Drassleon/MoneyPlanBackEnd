dba16fff64dee02cc9f9fdaa411006eaaa6122fd
Drassleon <toconasalvaro@gmail.com>
2019-06-21 23:24:26 -0500

    SubscriptionPayment Implementation
    Added Bussiness logic for Subscriptions
    Category link implemented
    SubscriptionPayment DTO created for easier creation of requests

--- src/main/java/pe/edu/upc/moneyplan/controller/SubscriptionPaymentControllerApi.java
+++ src/main/java/pe/edu/upc/moneyplan/controller/SubscriptionPaymentControllerApi.java
@@ -1,6 +1,5 @@
 package pe.edu.upc.moneyplan.controller;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +15,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.server.ResponseStatusException;
 
+import pe.edu.upc.moneyplan.models.entity.Client;
+import pe.edu.upc.moneyplan.models.entity.CustomCategory;
+import pe.edu.upc.moneyplan.models.entity.DefaultCategory;
 import pe.edu.upc.moneyplan.models.entity.SubscriptionPayment;
+import pe.edu.upc.moneyplan.service.impl.ClientService;
+import pe.edu.upc.moneyplan.service.impl.CustomCategoryService;
+import pe.edu.upc.moneyplan.service.impl.DefaultCategoryService;
 import pe.edu.upc.moneyplan.service.impl.SubscriptionPaymentService;
+import pe.edu.upc.moneyplan.service.inter.IClientService;
+import pe.edu.upc.moneyplan.service.inter.ICustomCategoryService;
+import pe.edu.upc.moneyplan.service.inter.IDefaultCategoryService;
 import pe.edu.upc.moneyplan.service.inter.ISubscriptionPaymentService;
+import pe.edu.upc.utils.SubscriptionPaymentDTO;
 
 @RestController
 @RequestMapping("/api/subscriptionPayment")
@@ -31,7 +40,13 @@ allowCredentials = "true")
 public class SubscriptionPaymentControllerApi {
 	@Autowired
 	ISubscriptionPaymentService subscriptionPaymentService = new SubscriptionPaymentService();
-
+	@Autowired
+	IDefaultCategoryService defaultCategoryService = new DefaultCategoryService();
+	@Autowired
+	ICustomCategoryService customCategoryService = new CustomCategoryService();
+	@Autowired
+	IClientService clientService = new ClientService();
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	@ResponseBody
 	public List<SubscriptionPayment> findAll() {
@@ -47,15 +62,43 @@ public class SubscriptionPaymentControllerApi {
 	}
 
 	@PostMapping("/")
-	public ResponseEntity<Object> create(@RequestBody SubscriptionPayment subscriptionPayment) {
+	public HttpStatus create(@RequestBody SubscriptionPaymentDTO subscriptionPayment) {
+		Client client = clientService.findById(subscriptionPayment.getClientId());		
+		if(client==null)
+		{
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Client not found"); 
+		}
+		SubscriptionPayment s = new SubscriptionPayment();
 
-		subscriptionPaymentService.save(subscriptionPayment);
+		s.setAmount(subscriptionPayment.getAmount());
+		s.setBillingDate(subscriptionPayment.getBillingDate());
+		s.setClient(client);
+		s.setDescription(subscriptionPayment.getDescription());
+		
+		DefaultCategory defaultCategory = defaultCategoryService.findByName(subscriptionPayment.getCategoryName());
+		if(defaultCategory!=null)
+		{
+			s.setDefaultCategory(defaultCategory);
+		}
+		else {
+			CustomCategory customCategory = customCategoryService.findByName(subscriptionPayment.getCategoryName(), client.getId());
+			if(customCategory!=null)
+			{
+				s.setCustomCategory(customCategory);
+			}
+			else
+			{
+				customCategory = new CustomCategory();
+				customCategory.setClient(client);
+				customCategory.setName(subscriptionPayment.getCategoryName());
+				customCategory.setDescription("");
+				customCategoryService.save(customCategory);
+				s.setCustomCategory(customCategory);
+			}
+		}
+		subscriptionPaymentService.save(s);
 
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(subscriptionPayment.getId()).toUri();
-
-		return ResponseEntity.created(location).build();
-
+		return HttpStatus.OK;
 	}
 
 	@PutMapping("/{id}")
---
+++ src/main/java/pe/edu/upc/utils/SubscriptionPaymentDTO.java
@@ -0,0 +1,45 @@
+package pe.edu.upc.utils;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class SubscriptionPaymentDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private Date billingDate;
+	private double amount;
+	private String description;
+	private Long clientId;
+	private String categoryName;
+	public Date getBillingDate() {
+		return billingDate;
+	}
+	public void setBillingDate(Date billingDate) {
+		this.billingDate = billingDate;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Long getClientId() {
+		return clientId;
+	}
+	public void setClientId(Long clientId) {
+		this.clientId = clientId;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+}
