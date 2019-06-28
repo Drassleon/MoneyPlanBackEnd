3e1d16318397520a2e434be0e58e939d4ea48fc9
Drassleon <toconasalvaro@gmail.com>
2019-06-27 22:38:10 -0500

    Task: Data Type changed for endpoint
    Changed return type from functions and responsebody from GET petitions.
    Now using TransactionDTO to secure data and easier reading.

--- src/main/java/pe/edu/upc/moneyplan/controller/TransactionControllerApi.java
+++ src/main/java/pe/edu/upc/moneyplan/controller/TransactionControllerApi.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,10 +35,10 @@ import pe.edu.upc.utils.TransactionDTO;
 
 @RestController
 @RequestMapping("/api/transaction")
-@CrossOrigin(
-methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT, RequestMethod.OPTIONS }, 
-allowedHeaders = { "Content-Type", "Authorization", "Access-Control-Allow-Headers", "X-Requested-With","Access-Control-Max-Age", "Access-Control-Allow-Methods", "Access-Control-Allow-Origin" }, 
-allowCredentials = "true")
+@CrossOrigin(methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT,
+		RequestMethod.OPTIONS }, allowedHeaders = { "Content-Type", "Authorization", "Access-Control-Allow-Headers",
+				"X-Requested-With", "Access-Control-Max-Age", "Access-Control-Allow-Methods",
+				"Access-Control-Allow-Origin" }, allowCredentials = "true")
 public class TransactionControllerApi {
 	@Autowired
 	ITransactionService transactionService = new TransactionService();
@@ -49,32 +48,98 @@ public class TransactionControllerApi {
 	ICustomCategoryService customCategoryService = new CustomCategoryService();
 	@Autowired
 	IClientService clientService = new ClientService();
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	@ResponseBody
-	public List<Transaction> findAll() {
-		List<Transaction> transactions = transactionService.findAll();
-
+	public List<TransactionDTO> findAll() {
+		List<Transaction> rawTransactions = transactionService.findAll();
+		if(rawTransactions.isEmpty())
+		{
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND,"List of transactions is empty!");
+		}
+		List<TransactionDTO> transactions = new ArrayList<TransactionDTO>();
+		for (Transaction rawTransaction : rawTransactions) {
+			TransactionDTO transaction = new TransactionDTO();
+			transaction.setAmount(rawTransaction.getAmount());
+			
+			if(rawTransaction.getCustomCategory()==null)
+			{
+				transaction.setCategoryName(rawTransaction.getDefaultCategory().getName());
+			}
+			else
+			{
+				transaction.setCategoryName(rawTransaction.getCustomCategory().getName());
+			}
+			
+			transaction.setClientId(rawTransaction.getClient().getId());
+			transaction.setDescription(rawTransaction.getDescription());
+			transaction.setTimestamp(rawTransaction.getTimestamp());
+			transaction.setTransactionType(rawTransaction.getTransactionType());
+			transactions.add(transaction);
+		}
 		return transactions;
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	@ResponseBody
-	public Transaction findOne(@PathVariable("id") Long id) {
-		return transactionService.findById(id);
+	public TransactionDTO findOne(@PathVariable("id") Long id) {
+		Transaction rawTransaction = transactionService.findById(id);
+		TransactionDTO transaction = new TransactionDTO();
+		transaction.setAmount(rawTransaction.getAmount());
+		
+		if(rawTransaction.getCustomCategory()==null)
+		{
+			transaction.setCategoryName(rawTransaction.getDefaultCategory().getName());
+		}
+		else
+		{
+			transaction.setCategoryName(rawTransaction.getCustomCategory().getName());
+		}
+		
+		transaction.setClientId(rawTransaction.getClient().getId());
+		transaction.setDescription(rawTransaction.getDescription());
+		transaction.setTimestamp(rawTransaction.getTimestamp());
+		transaction.setTransactionType(rawTransaction.getTransactionType());
+		return transaction;
 	}
 
 	@RequestMapping(value = "/client/{clientId}", method = RequestMethod.GET)
 	@ResponseBody
-	public List<Transaction> findByClientId(@PathVariable("clientId") Long clientId) {
-		return transactionService.findByClientId(clientId);
+	public List<TransactionDTO> findByClientId(@PathVariable("clientId") Long clientId) {
+		List<Transaction> rawTransactions = transactionService.findByClientId(clientId);
+		if(rawTransactions.isEmpty())
+		{
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND,"List of transactions is empty!");
+		}
+		List<TransactionDTO> transactions = new ArrayList<TransactionDTO>();
+		for (Transaction rawTransaction : rawTransactions) {
+			TransactionDTO transaction = new TransactionDTO();
+			transaction.setAmount(rawTransaction.getAmount());
+			
+			if(rawTransaction.getCustomCategory()==null)
+			{
+				transaction.setCategoryName(rawTransaction.getDefaultCategory().getName());
+			}
+			else
+			{
+				transaction.setCategoryName(rawTransaction.getCustomCategory().getName());
+			}
+			
+			transaction.setClientId(rawTransaction.getClient().getId());
+			transaction.setDescription(rawTransaction.getDescription());
+			transaction.setTimestamp(rawTransaction.getTimestamp());
+			transaction.setTransactionType(rawTransaction.getTransactionType());
+			transactions.add(transaction);
+		}
+		return transactions;
 	}
-	
-	@RequestMapping(value= "/resumen/gasto/categoria/{clientId}/{year}/{month}/",method = RequestMethod.GET)
+
+	@RequestMapping(value = "/resumen/gasto/categoria/{clientId}/{year}/{month}/", method = RequestMethod.GET)
 	@ResponseBody
-	public List<SummaryDTO> ExpenseByCategory(@PathVariable("clientId")Long clientId,@PathVariable("year")int year,@PathVariable("month")int month){
+	public List<SummaryDTO> ExpenseByCategory(@PathVariable("clientId") Long clientId, @PathVariable("year") int year,
+			@PathVariable("month") int month) {
 		List<SummaryDTO> result = new ArrayList<SummaryDTO>();
-		
+
 		List<DefaultCategory> defaultCategories = defaultCategoryService.findAll();
 		List<CustomCategory> customCategories = customCategoryService.findByClientId(clientId);
 		for (CustomCategory customCategory : customCategories) {
@@ -94,18 +159,15 @@ public class TransactionControllerApi {
 		for (Transaction expense : expenses) {
 			calendar.setTime(expense.getTimestamp());
 			String categoryname;
-			if(expense.getCustomCategory()==null)
-			{
+			if (expense.getCustomCategory() == null) {
 				categoryname = expense.getDefaultCategory().getName();
-			}
-			else
-			{
+			} else {
 				categoryname = expense.getCustomCategory().getName();
 			}
-			for(SummaryDTO summaryDTO: result)
-			{
-				if(summaryDTO.getCategoryName().equals(categoryname) && (expense.getTransactionType()==2 || expense.getTransactionType()==3) &&calendar.get(Calendar.MONTH)==month&& calendar.get(Calendar.YEAR)==year)
-				{
+			for (SummaryDTO summaryDTO : result) {
+				if (summaryDTO.getCategoryName().equals(categoryname)
+						&& (expense.getTransactionType() == 2 || expense.getTransactionType() == 3)
+						&& calendar.get(Calendar.MONTH) == month && calendar.get(Calendar.YEAR) == year) {
 
 					summaryDTO.addAmount(expense.getAmount());
 				}
@@ -113,12 +175,13 @@ public class TransactionControllerApi {
 		}
 		return result;
 	}
-	
-	@RequestMapping(value= "/resumen/ingreso/categoria/{clientId}/{year}/{month}/",method = RequestMethod.GET)
+
+	@RequestMapping(value = "/resumen/ingreso/categoria/{clientId}/{year}/{month}/", method = RequestMethod.GET)
 	@ResponseBody
-	public List<SummaryDTO> IncomeByCategory(@PathVariable("clientId")Long clientId,@PathVariable("year")int year,@PathVariable("month")int month){
+	public List<SummaryDTO> IncomeByCategory(@PathVariable("clientId") Long clientId, @PathVariable("year") int year,
+			@PathVariable("month") int month) {
 		List<SummaryDTO> result = new ArrayList<SummaryDTO>();
-		
+
 		List<DefaultCategory> defaultCategories = defaultCategoryService.findAll();
 		List<CustomCategory> customCategories = customCategoryService.findByClientId(clientId);
 		for (CustomCategory customCategory : customCategories) {
@@ -138,62 +201,64 @@ public class TransactionControllerApi {
 		for (Transaction income : incomes) {
 			calendar.setTime(income.getTimestamp());
 			String categoryname;
-			if(income.getCustomCategory()==null)
-			{
+			if (income.getCustomCategory() == null) {
 				categoryname = income.getDefaultCategory().getName();
-			}
-			else
-			{
+			} else {
 				categoryname = income.getCustomCategory().getName();
 			}
-			for(SummaryDTO summaryDTO: result)
-			{
-				if(summaryDTO.getCategoryName().equals(categoryname) && income.getTransactionType()==1 &&calendar.get(Calendar.MONTH)==month&& calendar.get(Calendar.YEAR)==year)
-				{
+			for (SummaryDTO summaryDTO : result) {
+				if (summaryDTO.getCategoryName().equals(categoryname) && income.getTransactionType() == 1
+						&& calendar.get(Calendar.MONTH) == month && calendar.get(Calendar.YEAR) == year) {
 					summaryDTO.addAmount(income.getAmount());
 				}
 			}
 		}
 		return result;
 	}
-	
+
 	@RequestMapping(value = "/resumen/gasto/promedio/{clientId}/{year}/{month}", method = RequestMethod.GET)
 	@ResponseBody
-	public double ExpenseAverage(@PathVariable("clientId") Long clientId,@PathVariable("year") int year ,@PathVariable("month") int month) {
+	public double ExpenseAverage(@PathVariable("clientId") Long clientId, @PathVariable("year") int year,
+			@PathVariable("month") int month) {
 		double averageSpent = 0.00f;
 		List<Transaction> expenses = transactionService.findByClientId(clientId);
 		Calendar calendar = Calendar.getInstance();
 		for (Transaction expense : expenses) {
 			calendar.setTime(expense.getTimestamp());
-			if (expense.getTransactionType() == 1 && (calendar.get(Calendar.YEAR)==year && calendar.get(Calendar.MONTH)== month))
+			if (expense.getTransactionType() == 1
+					&& (calendar.get(Calendar.YEAR) == year && calendar.get(Calendar.MONTH) == month))
 				averageSpent += expense.getAmount();
 		}
 		return averageSpent / expenses.size();
 	}
-	
+
 	@RequestMapping(value = "/expenses/{clientId}/{year}/{month}", method = RequestMethod.GET)
 	@ResponseBody
-	public double ExpensePerMonth(@PathVariable("clientId") Long clientId, @PathVariable("year") int year, @PathVariable("month") int month) {
+	public double ExpensePerMonth(@PathVariable("clientId") Long clientId, @PathVariable("year") int year,
+			@PathVariable("month") int month) {
 		double totalExpensesFromMonth = 0.00f;
 		List<Transaction> expenses = transactionService.findByClientId(clientId);
 		for (Transaction expense : expenses) {
 			Calendar calendar = Calendar.getInstance();
 			calendar.setTime(expense.getTimestamp());
-			if (expense.getTransactionType() == 2 && calendar.get(Calendar.YEAR) == year && calendar.get(Calendar.MONTH) == month)
+			if (expense.getTransactionType() == 2 && calendar.get(Calendar.YEAR) == year
+					&& calendar.get(Calendar.MONTH) == month)
 				totalExpensesFromMonth += expense.getAmount();
 		}
 		return totalExpensesFromMonth;
 	}
-	
+
 	@RequestMapping(value = "/incomes/{clientId}/{year}/{month}", method = RequestMethod.GET)
 	@ResponseBody
-	public double IncomePerMonth(@PathVariable("clientId") Long clientId, @PathVariable("year") int year, @PathVariable("month") int month) {
+	public double IncomePerMonth(@PathVariable("clientId") Long clientId, @PathVariable("year") int year,
+			@PathVariable("month") int month) {
 		double totalIncomesFromMonth = 0.00f;
 		List<Transaction> incomes = transactionService.findByClientId(clientId);
 		for (Transaction income : incomes) {
 			Calendar calendar = Calendar.getInstance();
 			calendar.setTime(income.getTimestamp());
-			if (income.getTransactionType() == 1 && calendar.get(Calendar.YEAR) == year && calendar.get(Calendar.MONTH) == month )
+			if (income.getTransactionType() == 1 && calendar.get(Calendar.YEAR) == year
+					&& calendar.get(Calendar.MONTH) == month)
 				totalIncomesFromMonth += income.getAmount();
 		}
 		return totalIncomesFromMonth;
@@ -202,9 +267,8 @@ public class TransactionControllerApi {
 	@PostMapping("/")
 	public HttpStatus create(@RequestBody TransactionDTO transaction) {
 		Client client = clientService.findById(transaction.getClientId());
-		if(client==null)
-		{
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Client not found"); 
+		if (client == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Client not found");
 		}
 		Transaction t = new Transaction();
 		t.setAmount(transaction.getAmount());
@@ -213,18 +277,14 @@ public class TransactionControllerApi {
 		t.setTimestamp(transaction.getTimestamp());
 		t.setTransactionType(transaction.getTransactionType());
 		DefaultCategory defaultCategory = defaultCategoryService.findByName(transaction.getCategoryName());
-		if(defaultCategory!=null)
-		{
+		if (defaultCategory != null) {
 			t.setDefaultCategory(defaultCategory);
-		}
-		else {
-			CustomCategory customCategory = customCategoryService.findByName(transaction.getCategoryName(), client.getId());
-			if(customCategory!=null)
-			{
+		} else {
+			CustomCategory customCategory = customCategoryService.findByName(transaction.getCategoryName(),
+					client.getId());
+			if (customCategory != null) {
 				t.setCustomCategory(customCategory);
-			}
-			else
-			{
+			} else {
 				customCategory = new CustomCategory();
 				customCategory.setClient(client);
 				customCategory.setName(transaction.getCategoryName());
@@ -238,18 +298,41 @@ public class TransactionControllerApi {
 		return HttpStatus.OK;
 	}
 
-	@PutMapping("/{id}")
-	public ResponseEntity<Object> update(@RequestBody Transaction transaction, @PathVariable long id) {
+	@PutMapping("/{transactionId}")
+	public HttpStatus update(@RequestBody TransactionDTO transaction, @PathVariable("transactionId") long id) {
+
+		Client client = clientService.findById(transaction.getClientId());
+		if (client == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Client not found");
+		}
 
 		Transaction transactionAux = transactionService.findById(id);
+
 		if (transactionAux != null) {
-			transaction.setId(id);
-			transactionService.save(transaction);
-			return ResponseEntity.noContent().build();
+			transactionAux.setAmount(transaction.getAmount());
+			transactionAux.setDescription(transaction.getDescription());
 
+			DefaultCategory defaultCategory = defaultCategoryService.findByName(transaction.getCategoryName());
+			if (defaultCategory != null) {
+				transactionAux.setDefaultCategory(defaultCategory);
+			} else {
+				CustomCategory customCategory = customCategoryService.findByName(transaction.getCategoryName(),
+						client.getId());
+				if (customCategory != null) {
+					transactionAux.setCustomCategory(customCategory);
+				} else {
+					customCategory = new CustomCategory();
+					customCategory.setClient(client);
+					customCategory.setName(transaction.getCategoryName());
+					customCategory.setDescription("");
+					customCategoryService.save(customCategory);
+					transactionAux.setCustomCategory(customCategory);
+				}
+			}
+			transactionService.save(transactionAux);
+			return HttpStatus.OK;
 		}
-		return ResponseEntity.notFound().build();
-
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Transaction not found!");
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
